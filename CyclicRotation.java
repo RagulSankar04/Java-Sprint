6f492ececf90c6c5bce45022ab8cfbe69f6fca34
Ragulsankar04 <ragulsankar2004@gmail.com>
2025-05-13 20:14:43 +0530

    Arrays 6-8 Completed

---
+++ CyclicRotation.java
@@ -0,0 +1,19 @@
+
+public class CyclicRotation {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+        
+        System.out.print("Rotated Array: ");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
